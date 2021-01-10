73f0f7001b1cce0a04a518edb9171b4335839edc
simyeen <simyeen@naver.com>
2021-01-10 21:10:36 +0900

    update

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import TodoTemplate from './components/TodoTemplate';
 
 class App extends Component {
   render() {
-    return <div>Todo App을 만들자! </div>;
+    return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
   }
 }
 
