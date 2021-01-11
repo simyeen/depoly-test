a3ed5d2434f306b54e59cd07c05968ea20bd25d7
simyeen <simyeen@naver.com>
2021-01-11 14:14:23 +0900

    fear : styling todo

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 class App extends Component {
   render() {
-    return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+    return (
+      <TodoTemplate>
+        <TodoInsert />
+        <TodoList />
+      </TodoTemplate>
+    );
   }
 }
 
