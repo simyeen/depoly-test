be5090dfd253cbfaa58b5253bb55a79d874a5d97
simyeen <simyeen@naver.com>
2021-01-12 16:06:11 +0900

    feat : add url parmas

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
   return (
@@ -16,9 +17,20 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
       </ul>
+      <ul>
+        <li>
+          <Link to="/profile/veloport">veloport</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/profile/gildong">gildong</Link>
+        </li>
+      </ul>
       <hr />
       <Route path="/" component={Home} exact={true}></Route>
       <Route path={['/about', '/info']} component={About}></Route>
+      <Route path="/profile/:username" component={Profile}></Route>
     </div>
   );
 };
---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const data = {
+  veloport: { name: '김민준', description: '리액트를 좋아하는 개발자' },
+  gildong: { name: '홍길동', description: '고전 소설 홍길동전의 주인공' },
+};
+
+const Profile = ({ match }) => {
+  const { username } = match.params; // router를 통해서 받은 params
+  const profile = data[username]; // 이 컴퍼넌트에 존재하는 정보.
+
+  if (!profile) {
+    return <div>존재하지 않는 사용자입니다.</div>;
+  }
+
+  return (
+    <div>
+      <h3>
+        ?{username}({profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+};
+
+export default Profile;
