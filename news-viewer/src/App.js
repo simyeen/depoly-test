c610f9ff86830253e1a9a30b55c3a3bd3b0304d0
simyeen <simyeen@naver.com>
2021-01-14 22:42:48 +0900

    a

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+// http://newsapi.org/v2/top-headlines?country=kr&apiKey=87382549776b4e98962b103f32bc5e57
+
 const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=87382549776b4e98962b103f32bc5e57',
       );
       setData(response.data);
     } catch (e) {
