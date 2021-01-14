5070684dd38c9c884bb4582cbea9bf499619d577
simyeen <simyeen@naver.com>
2021-01-14 23:52:38 +0900

    NewItemsBlock.js

---
+++ news-viewer/src/components/NewItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewItemsBlock = () => {
+  return <div></div>;
+};
+
+export default NewItemsBlock;
+
+const NewItemsBlock = styled.div`
+  display: flex;
+`;
