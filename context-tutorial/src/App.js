1c02bd7ab55e8e680c4f959edeb16ef05092977a
simyeen <simyeen@naver.com>
2021-01-17 03:32:31 +0900

    feat : add useContext

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import SelectColor from './components/SelectColor';
 import { ColorProvider } from './contexts/color';
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColor></SelectColor>
         <ColorBox></ColorBox>
       </div>
     </ColorProvider>
--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, { useContext } from 'react';
+import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
+
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <div>
-          <div
-            style={{
-              width: '64px',
-              height: '64px',
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: '32px',
-              height: '32px',
-              background: state.subcolor,
-            }}
-          />
-        </div>
-      )}
-    </ColorConsumer>
+    <div>
+      <div
+        style={{
+          width: '64px',
+          height: '64px',
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subcolor,
+        }}
+      />
+    </div>
   );
 };
 
