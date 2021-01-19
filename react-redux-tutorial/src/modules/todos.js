eaba33a72856b20f42873464efcffbd3d004506e
simyeen <simyeen@naver.com>
2021-01-19 15:21:05 +0900

    todos.js

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -3,14 +3,14 @@ const INSERT = 'todos/INSERT ';
 const TOGGLE = 'todos/TOGGLE ';
 const REMOVE = 'todos/REMOVE ';
 
-export const changeInput = (input) => ({ type: 'CHANGE_INPUT', input });
+export const changeInput = (input) => ({ type: CHANGE_INPUT, input });
 let id = 3;
 export const insert = (text) => ({
-  type: 'INSERT',
+  type: INSERT,
   todo: { text, id: id++, done: false },
 });
-export const toggle = (id) => ({ type: 'TOGGLE', id });
-export const remove = (id) => ({ type: 'REMOVE', id });
+export const toggle = (id) => ({ type: TOGGLE, id });
+export const remove = (id) => ({ type: REMOVE, id });
 
 const initialState = {
   input: '',
