0d9b99ed2d627dc5a503b19f1ef168448509bf4f
simyeen <simyeen@naver.com>
2021-01-22 01:15:56 +0900

    createRequestSaga

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -2,6 +2,7 @@ import { createAction, handleActions } from 'redux-actions';
 import { call, put, takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
 import { startLoading, finishLoading } from './loading';
+import createRequestSaga from '../lib/createRequestSaga';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -14,41 +15,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST));
-  try {
-    const post = yield call(api.getPost, action.payload);
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_POST));
-}
-
-function* getUsersSaga() {
-  yield put(startLoading(GET_USERS));
-  try {
-    const users = yield call(api.getUsers);
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_USERS));
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 export function* sampleSaga() {
   yield takeLatest(GET_POST, getPostSaga);
