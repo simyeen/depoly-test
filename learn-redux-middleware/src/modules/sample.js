bf5b6fda2b4a6a7500df23f3ebe1c9446b2859c8
simyeen <simyeen@naver.com>
2021-01-21 12:45:13 +0900

    feat : SampleContainer & SamplePresenter.js

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -13,9 +13,16 @@ export const getPost = (id) => async (dispatch) => {
   dispatch({ type: GET_POST });
   try {
     const response = await api.getPost(id);
-    dispatch({ type: GET_POST_SUCCESS, payload: response.data });
+    dispatch({
+      type: GET_POST_SUCCESS,
+      payload: response.data,
+    });
   } catch (e) {
-    dispatch({ type: GET_POST_FAILURE, payload: e, error: true });
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
     throw e;
   }
 };
@@ -24,9 +31,16 @@ export const getUsers = () => async (dispatch) => {
   dispatch({ type: GET_USERS });
   try {
     const response = await api.getUsers();
-    dispatch({ type: GET_USERS_SUCCESS, payload: response.data });
+    dispatch({
+      type: GET_USERS_SUCCESS,
+      payload: response.data,
+    });
   } catch (e) {
-    dispatch({ type: GET_USERS_FAILURE, payload: e, error: true });
+    dispatch({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true,
+    });
     throw e;
   }
 };
@@ -40,39 +54,59 @@ const initialState = {
   users: null,
 };
 
-const sample = handleActions({
-  [GET_POST]: (state) => ({
-    ...state,
-    loading: { ...state.loading, GET_POST: true },
-  }),
+const sample = handleActions(
+  {
+    [GET_POST]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: true,
+      },
+    }),
 
-  [GET_POST_SUCCESS]: (state, action) => ({
-    ...state,
-    loading: { ...state.loading, GET_POST: false },
-    post: action.payload,
-  }),
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: false,
+      },
+      post: action.payload,
+    }),
 
-  [GET_POST_FAILURE]: (state, action) => ({
-    ...state,
-    loading: { ...state.loading, GET_POST: false },
-  }),
+    [GET_POST_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: false,
+      },
+    }),
 
-  [GET_USERS]: (state) => ({
-    ...state,
-    loading: { ...state.loading, GET_USERS: true },
-  }),
+    [GET_USERS]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: true,
+      },
+    }),
 
-  [GET_USERS_SUCCESS]: (state, action) => ({
-    ...state,
-    loading: { ...state.loading, GET_USERS: false },
-    users: action.payload,
-  }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false,
+      },
+      users: action.payload,
+    }),
 
-  [GET_USERS_FAILURE]: (state, action) => ({
-    ...state,
-    loading: { ...state.loading, GET_USERS: false },
-  }),
-  initialState,
-});
+    [GET_USERS_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false,
+      },
+    }),
+  },
+  initialState
+);
 
 export default sample;
