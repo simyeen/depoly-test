cd01a921819ed9a3f6ddbd4ba5ed64edc787b90b
simyeen <simyeen@naver.com>
2021-01-21 10:26:22 +0900

    applying redux-thunk(double arrow func)

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -8,7 +8,7 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
-import { ReduxThunk } from 'redux-thunk';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger();
 const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
