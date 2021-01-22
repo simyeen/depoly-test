78a685ccd83f0ef5f222b9c80dd2adfb53e9f2e0
simyeen <simyeen@naver.com>
2021-01-22 16:40:21 +0900

    React.lazy

--- splittin-sample/src/App.js
+++ splittin-sample/src/App.js
@@ -1,25 +1,29 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { SplitMe: null };
+
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="APP">
+        <header className="APP-header">
+          <img src={logo} className="APP-logo" alt="logo" />
+          <p onClick={this.handleClick}> Hello React! 빵 들고가기</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
