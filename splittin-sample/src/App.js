9d4c008d90e5f14158b14be875aa098f74a4adf8
simyeen <simyeen@naver.com>
2021-01-23 15:44:59 +0900

    test : build/static/js

--- splittin-sample/src/App.js
+++ splittin-sample/src/App.js
@@ -1,29 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  state = { SplitMe: null };
-
-  handleClick = async () => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default,
-    });
-  };
-
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="APP">
-        <header className="APP-header">
-          <img src={logo} className="APP-logo" alt="logo" />
-          <p onClick={this.handleClick}> Hello React! 빵 들고가기</p>
-          {SplitMe && <SplitMe />}
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="APP">
+      <header className="APP-header">
+        <img src={logo} className="APP-logo" alt="logo" />
+        <p onClick={this.handleClick}> Hello React! 빵 들고가기</p>
+        {SplitMe && <SplitMe />}
+      </header>
+    </div>
+  );
+};
 
 export default App;
