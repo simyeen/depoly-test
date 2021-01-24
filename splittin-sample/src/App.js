9ae823ca9287c4db286b88d582656d2d655ab954
simyeen <simyeen@naver.com>
2021-01-25 01:05:09 +0900

    loadable/components

--- splittin-sample/src/App.js
+++ splittin-sample/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, Suspense } from 'react';
+import loadable from '@loadable/component';
 import logo from './logo.svg';
 import './App.css';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>,
+});
 
 const App = () => {
   const [visible, setVisible] = useState(false);
@@ -13,9 +16,7 @@ const App = () => {
       <header className="APP-header">
         <img src={logo} className="APP-logo" alt="logo" />
         <p onClick={onClick}> Hello React! 빵 들고가기</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
