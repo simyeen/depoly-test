aea840de95ebb16aa2b7de99a9d0a066b0cec8d4
simyeen <simyeen@naver.com>
2021-01-24 23:57:30 +0900

    setting for Suspensse

--- splittin-sample/src/App.js
+++ splittin-sample/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, Suspense } from 'react';
+import logo from './logo.svg';
+import './App.css';
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
   return (
     <div className="APP">
       <header className="APP-header">
         <img src={logo} className="APP-logo" alt="logo" />
-        <p onClick={this.handleClick}> Hello React! 빵 들고가기</p>
-        {SplitMe && <SplitMe />}
-        {SplitMe && <SplitMe />}
+        <p onClick={onClick}> Hello React! 빵 들고가기</p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
