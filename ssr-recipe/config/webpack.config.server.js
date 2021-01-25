b020954c90761eab7f8bc451b3d120f8f17bd899
simyeen <simyeen@naver.com>
2021-01-25 16:58:42 +0900

    webpack setting for css, sass

--- ssr-recipe/config/webpack.config.server.js
+++ ssr-recipe/config/webpack.config.server.js
@@ -1,5 +1,11 @@
 const paths = require('./paths');
 const path = require('./paths');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
 
 module.exports = {
   mode: 'production',
@@ -11,5 +17,108 @@ module.exports = {
     filename: 'server.js',
     chunkFilenmae: 'js/[name].chunk.js',
     publicPath: path.publicUrlOrPath,
+  },
+
+  modules: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+              customize: require.resolve(
+                'babel-preset-react-app/webpack-overrides'
+              ),
+              plugins: [
+                [
+                  require.resolve('babel-plugin-named-asset-import'),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent: '@svgr/webpack?-svgo![path]',
+                      },
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+          {
+            test: cssRegex,
+            exclude: cssModuleRegex,
+            loader: require.resolve('css-loader'),
+            options: {
+              onlyLocals: true,
+            },
+          },
+          {
+            test: cssModuleRegex,
+            loader: require.resolve('css-loader'),
+            options: {
+              modules: true,
+              onlyLocals: true,
+              getLocalIdent: getCSSModuleLocalIdent,
+            },
+          },
+          //Sass처리
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  onlyLocals: true,
+                },
+              },
+              require.resolve('sass-loader'),
+            ],
+          },
+          //Sass + CSS 모듈
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  modules: true,
+                  onlyLocals: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                },
+              },
+              require.resolve('sass-loader'),
+            ],
+          },
+
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('css-loader'),
+            options: {
+              emitFile: false,
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+
+          {
+            loader: require.resolve('file-loader'),
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              emitFile: false,
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    module: ['node-modules'],
   },
 };
