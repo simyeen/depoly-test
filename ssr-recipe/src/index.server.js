520e867ae51e28e634d70f7e2a868792c0e10fa6
simyeen <simyeen@naver.com>
2021-01-25 22:56:05 +0900

    ssr setting compelete

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -3,6 +3,44 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import { StaticRouter } from 'react-router-dom';
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter((key) => /chunk\.js$/.exec(key))
+  .map((key) => `<script src = "${manifest.files[key]}"> </script>`)
+  .join('');
+
+function createPage(root) {
+  return ` <!DOCTYPE html>
+    <html lang ="en">
+    <head>
+      <meta charset = "utf-8"/>
+      <link rel = "shortcut icon" href="favicon.ico"/>
+      <meta
+        name ="viewport"
+        content = "width=device-width, initial-scale=1, shrink-to-fit=no"/>
+      <meta name ="theme-color" content="#000000"/>
+      <title> React App</title>
+      <link href = "${manifest.files['main.css']}" rel = "stylesheet"/>
+    </head>
+      
+      <body>
+       <noscript> You need to enable JavaScript to run tis app. </noscript>
+       <div id ="root">
+        ${root}
+       </div>
+       <script src="${manifest.files['runtime~main.js']}"> </script >
+       ${chunks} 
+        <script src="${manifest.files['main.js']}"> </script >
+      </body>
+      </html>
+    `;
+}
 
 const app = express();
 
@@ -14,9 +52,14 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고,
-  res.send(root); // Client에게 결과물을 응답합니다.
+  res.send(createPage(root)); // Client에게 결과물을 응답합니다.
 };
 
+const serve = express.static(path.resolve('./build'), {
+  index: false,
+});
+
+app.use(serve);
 app.use(serverRender);
 
 app.listen(5000, () => {
