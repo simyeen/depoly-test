38fbd8ab97fbc0ac5028517d5aaf4054806f057f
simyeen <simyeen@naver.com>
2021-01-25 18:12:46 +0900

    fix : reinstall node_modules cuz yarn eject and yarn add express

--- ssr-recipe/config/webpack.config.server.js
+++ ssr-recipe/config/webpack.config.server.js
@@ -1,25 +1,28 @@
+const nodeExternals = require('webpack-node-externals');
 const paths = require('./paths');
-const path = require('./paths');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+const webpack = require('webpack');
+const getClientEnvironment = require('./env');
 
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+
 module.exports = {
   mode: 'production',
   entry: paths.ssrIndexJs,
   target: 'node',
-
   output: {
     path: paths.ssrBuild,
     filename: 'server.js',
-    chunkFilenmae: 'js/[name].chunk.js',
-    publicPath: path.publicUrlOrPath,
+    chunkFilename: 'js/[name].chunk.js',
+    publicPath: paths.publicUrlOrPath,
   },
 
-  modules: {
+  module: {
     rules: [
       {
         oneOf: [
@@ -98,7 +101,7 @@ module.exports = {
 
           {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-            loader: require.resolve('css-loader'),
+            loader: require.resolve('url-loader'),
             options: {
               emitFile: false,
               limit: 10000,
@@ -119,6 +122,8 @@ module.exports = {
     ],
   },
   resolve: {
-    module: ['node-modules'],
+    modules: ['node-modules'],
   },
+  externals: [nodeExternals()],
+  plugins: [new webpack.DefinePlugin(env.stringified)],
 };
