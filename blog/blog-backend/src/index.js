508c032641e35e81503d45b7c36dd05ff448b863
simyeen <simyeen@naver.com>
2021-01-27 23:29:10 +0900

    next return Promise and using then

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -5,7 +5,14 @@ const app = new Koa();
 app.use((ctx, next) => {
   console.log(ctx.url);
   console.log(1);
-  next();
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401;
+    return;
+  }
+
+  next().then(() => {
+    console.log('END');
+  });
 });
 
 app.use((ctx, next) => {
