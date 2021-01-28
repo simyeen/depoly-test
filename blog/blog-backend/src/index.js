c3254bf6f6d9a5aad8ae295fa74d8f313482f522
simyeen <simyeen@naver.com>
2021-01-28 16:06:57 +0900

    backend - koa

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
@@ -8,6 +9,7 @@ const router = new Router();
 
 router.use('/api', api.routes());
 
+app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
