9ba77ffe310f62272efee4bd7460c845c56cf3f6
simyeen <simyeen@naver.com>
2021-01-31 23:25:39 +0900

    Ui Loing & Register Pages

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -42,13 +42,24 @@ const Footer = styled.div`
   }
 `;
 
-const AuthForm = () => {
+const ButtonWithMarginTop = styled(Button)`
+  margin-top: 1rem;
+`;
+
+const textMap = {
+  login: '로그인',
+  register: '회원가입',
+};
+
+const AuthForm = ({ type }) => {
+  const text = textMap[type];
+
   return (
     <AuthFormBlock>
-      <h3>로그인</h3>
+      <h3>{text}</h3>
       <form>
         <StyledInput
-          autoComplete="uername"
+          autoComplete="username"
           name="username"
           placeholder="아이디"
         />
@@ -58,9 +69,25 @@ const AuthForm = () => {
           placeholder="비밀번호"
           type="password"
         />
+        {type === 'register' && (
+          <StyledInput
+            autoComplete="new-password"
+            name="password"
+            placeholder="비밀번호 확인"
+            type="password"
+          />
+        )}
+
+        <ButtonWithMarginTop cyan fullWidth>
+          {text}
+        </ButtonWithMarginTop>
       </form>
       <Footer>
-        <Link to="/register">회원가입</Link>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </Footer>
     </AuthFormBlock>
   );
