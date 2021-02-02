0571fb39d9e9011d0d61da4e18986b6bf7e36c62
simyeen <simyeen@naver.com>
2021-02-02 12:36:36 +0900

    Feat : add lib/loaing for reuse

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -1,9 +1,11 @@
 import {} from 'redux';
 import { combineReducers } from 'redux';
 import auth from './auth';
+import loading from './loading';
 
 const rootReducer = combineReducers({
   auth,
+  loading,
 });
 
 export default rootReducer;
