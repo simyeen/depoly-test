7658aa5f0ea6643da35d1301d4b7e9ed20562aae
simyeen <simyeen@naver.com>
2021-02-06 15:58:22 +0900

    testging deploy

---
+++ blog/blog-frontend/src/containers/write/WriteActionButtonsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import WriteActionButtons from '../../components/write/WriteActionButtons';
+
+const WriteActionButtonsContainer = () => {
+  return <WriteActionButtons />;
+};
+
+export default WriteActionButtonsContainer;
